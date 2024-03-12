45e4c70c7b66b5001f21260d2d2d77dc85d84a51
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2024-03-12 00:13:15 -0300

    register and login ready

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at,
+            'token' => $this->token,
+        ];
+        //return parent::toArray($request);
+    }
+}
